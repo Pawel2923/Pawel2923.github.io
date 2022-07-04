e13cfc2d9dca6313e917ad403d12476693a3d83c
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2022-07-04 15:47:54 +0200

    Poprawki w stylach i strukturze

--- src/components/Main/Home/Sections/Services/Services.js
+++ src/components/Main/Home/Sections/Services/Services.js
@@ -1,30 +1,18 @@
 import React from "react";
 import styles from "./Services.module.css";
 import trim from "./img/trim.jpg";
-import haircut from "./img/haircut.jpg";
-import products from "./img/products.jpg";
-import barber from "./img/barber.jpg";
+// import haircut from "./img/haircut.jpg";
+// import products from "./img/products.jpg";
+// import barber from "./img/barber.jpg";
 
 const Services = (props) => {
   return (
     <section className={`${props.className} ${styles.services}`} id="services">
-      <h1>Usługi</h1>
+      <h1>Nasze usługi</h1>
       <ul className={styles.list}>
         <li className={styles["list-item"]}>
-          <span>Strzyżenie i modelowanie zarostu</span>
-          <img src={trim} alt="Zarost" className={styles.image} />
-        </li>
-        <li className={styles["list-item"]}>
-          <span>Strzyżenie i stylizowanie fryzury</span>
-          <img src={haircut} alt="Fryzury" className={styles.image} />
-        </li>
-        <li className={styles["list-item"]}>
-          <span>Zabiegi pielęgnacyjne</span>
-          <img src={products} alt="Pielęgnacja" className={styles.image} />
-        </li>
-        <li className={styles["list-item"]}>
-          <span>Doradztwo w zakresie pielęgnacji i stylizacji zarostu</span>
-          <img src={barber} alt="Doradztwo" className={styles.image} />
+          <img src={trim} alt="Strzyżenie zdjęcie" className={styles.image} />
+          <h3>Strzyżenie i modelowanie zarostu</h3>
         </li>
       </ul>
     </section>
