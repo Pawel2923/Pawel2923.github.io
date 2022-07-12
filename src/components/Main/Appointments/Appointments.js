869069d9bbdb42aed575f16e2f11720aad88f797
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2022-07-12 22:43:40 +0200

    Updates

--- src/components/Main/Appointments/Appointments.js
+++ src/components/Main/Appointments/Appointments.js
@@ -1,5 +1,6 @@
-import Button from "components/UI/Button/Button";
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import Button from "components/UI/Button/Button";
 import styles from "./Appointments.module.css";
 
 const checkValidity = (type, value) => {
@@ -74,7 +75,7 @@ const Services = () => {
 
   const phoneChangeHandler = (ev) => {
     setPhoneNumber(ev.target.value);
-    
+
     if (!ev.target.validity.valid) {
       ev.target.classList.add("invalid");
     } else {
@@ -84,7 +85,7 @@ const Services = () => {
 
   const messageChangeHandler = (ev) => {
     setMessage(ev.target.value);
-    
+
     if (!ev.target.validity.valid) {
       ev.target.classList.add("invalid");
     } else {
@@ -93,6 +94,8 @@ const Services = () => {
   };
 
   const validateForm = () => {
+    let isValid = false;
+
     const nameInput = document.getElementById("name");
     const emailInput = document.getElementById("email");
     const phoneInput = document.getElementById("phoneNumber");
@@ -102,51 +105,80 @@ const Services = () => {
       if (messageInput.classList.contains("invalid")) {
         messageInput.classList.remove("invalid");
       }
+      isValid = true;
     } else {
       if (!messageInput.classList.contains("invalid")) {
         messageInput.classList.add("invalid");
         messageInput.focus();
       }
+      isValid = false;
     }
 
     if (checkValidity("phoneNumber", phoneNumber)) {
       if (phoneInput.classList.contains("invalid")) {
         phoneInput.classList.remove("invalid");
       }
+      isValid = true;
     } else {
       if (!phoneInput.classList.contains("invalid")) {
         phoneInput.classList.add("invalid");
         phoneInput.focus();
       }
+      isValid = false;
     }
 
     if (checkValidity("email", email)) {
       if (emailInput.classList.contains("invalid")) {
         emailInput.classList.remove("invalid");
       }
+      isValid = true;
     } else {
       if (!emailInput.classList.contains("invalid")) {
         emailInput.classList.add("invalid");
         emailInput.focus();
       }
+      isValid = false;
     }
 
     if (checkValidity("text", name)) {
       if (nameInput.classList.contains("invalid")) {
         nameInput.classList.remove("invalid");
       }
+      isValid = true;
     } else {
       if (!nameInput.classList.contains("invalid")) {
         nameInput.classList.add("invalid");
         nameInput.focus();
       }
+      isValid = false;
     }
+
+    return isValid;
   };
 
   const submitHandler = (ev) => {
     ev.preventDefault();
 
-    validateForm();
+    if (validateForm()) {
+      emailjs.send(
+        "service_mt81aag",
+        "template_j69osgm",
+        {
+          from_name: name,
+          from_email: email,
+          reply_to: email,
+          message: message,
+          phone_number: phoneNumber
+        },
+        "vSOVWrVLzBl721pXk"
+      ).then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.error(error.text);
+      });
+    } else {
+      console.warn("Błąd walidacji maila");
+    }
 
     setName("");
     setEmail("");
