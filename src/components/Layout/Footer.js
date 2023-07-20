a18d1738756b22b545b0237f9b4f5cceee3141fb
Paweł <pawelporemba123@gmail.com>
2023-07-20 16:30:02 +0200

    Change footer positioning method

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,31 +1,9 @@
-import { useEffect } from "react";
-
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-  useEffect(() => {
-    const changeFooterPosition = (bodyHeight) => {
-      if (!(window.innerHeight <= bodyHeight)) {
-        document
-          .getElementsByClassName(classes.footer)[0]
-          .classList.add(classes.absolute);
-      } else {
-        document
-          .getElementsByClassName(classes.footer)[0]
-          .classList.remove(classes.absolute);
-      }
-    };
-
-    const resizeObserver = new ResizeObserver((entries) =>
-      changeFooterPosition(entries[0].target.clientHeight)
-    );
-
-    resizeObserver.observe(document.body);
-  }, []);
-
   return (
     <footer className={classes.footer}>
-      <p>Wszelkie prawa zastrzeżone &copy; 2022</p>
+      <p>Barber Shop - projekt w React.js by Paweł Poremba</p>
     </footer>
   );
 };
